186c5fe05dce1ddd4310b02af65968ebb29432da
Leticia Sartori <leticia.m.sartori@usp.br>
2023-10-09 20:43:35 -0300

    adicionando ex 6

---
+++ Python04_6.1.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+#6.Itere cada elemento desta lista usando um forloop: [101,2,15,22,95,33,2,27,72,15,52]
+
+lista = [101,2,15,22,95,33,2,27,72,15,52]
+
+for x in lista:
+	print (x)
---
+++ Python04_6.2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+#Imprima apenas os valores pares (dica: use o operador módulo) da lista [101,2,15,22,95,33,2,27,72,15,52].
+
+lista = [101,2,15,22,95,33,2,27,72,15,52]
+
+for x in lista:
+	if x % 2 == 0:
+		print (x)

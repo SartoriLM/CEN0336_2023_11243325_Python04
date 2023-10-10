ea76b4af3b050f501f6575b380134c1cad2f6255
Leticia Sartori <leticia.m.sartori@usp.br>
2023-10-09 21:15:57 -0300

    add ex 8

---
+++ Python04_8.1.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+#Write a script that uses range() in a for loop to print out every number between 0 and 99
+
+for x in range (0, 100):
+	print (x)
---
+++ Python04_8.2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+#Modify your loop to print out every number between 1 and 100.
+
+for x in range (1, 101):
+	print (x)
+
+

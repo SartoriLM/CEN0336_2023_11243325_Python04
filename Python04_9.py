dc372d4ec047b1d5d95bf1f37ab1f771d29e8223
Leticia Sartori <leticia.m.sartori@usp.br>
2023-10-10 22:23:50 -0300

    s

--- Python04_9.py
+++ Python04_9.py
@@ -2,7 +2,11 @@
 
 #Create a new script that uses list comprehension to do the same thing as problem 8. (Use range() to print out every number between 1 and 100.)
 
+#usando list comprehension
 list = [x for x in range (1, 101)] 
-print (list)
+print ("list:", list)
 
+#imprimindo cada elemento da lista como no exercício 8, como solicitado
+for x in list:
+	print ("list elements:", x)
 
